313ade018e6e32ee1d0112d34109ec12f991a1f3
hjm113 <63735064+hjm113@users.noreply.github.com>
2023-05-01 15:11:40 +0900

    [Silver III] Title: 2×n 타일링 2, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Silver/11727. 2×n 타일링 2/2×n 타일링 2.cc
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <queue>
+#include <stack>
+#include <tuple>
+#include <deque>
+#include <string>
+#include <algorithm>
+#include <climits>
+#include <cmath>
+using namespace std;
+using ll = long long;
+#define X first
+#define Y second
+int dp[1005];
+int n, m;
+int t;
+int main(void){
+  ios::sync_with_stdio(0);
+  cin.tie(0);
+  cin >> n;
+  dp[1] = 1;
+  dp[2] = 3;
+  for(int i = 3; i <= n; i++) {
+    dp[i] = (dp[i-1] + (dp[i-2]*2)) % 10007;
+  }
+  cout << dp[n];
+}
