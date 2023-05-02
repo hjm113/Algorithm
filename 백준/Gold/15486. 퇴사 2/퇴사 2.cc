0d10d68e5e331004953695222e9bb2affc995b5e
hjm113 <63735064+hjm113@users.noreply.github.com>
2023-05-02 14:21:25 +0900

    [Gold V] Title: 퇴사 2, Time: 236 ms, Memory: 31316 KB -BaekjoonHub

---
+++ 백준/Gold/15486. 퇴사 2/퇴사 2.cc
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <queue>
+#include <stack>
+#include <tuple>
+#include <deque>
+#include <string>
+#include <algorithm>
+#include <climits>
+#include <cmath>
+using namespace std;
+using ll = long long;
+#define X first
+#define Y second
+int t[1500005];
+long long p[1500005];
+long long dp[1500005];
+int n;
+int main(void){
+  ios::sync_with_stdio(0);
+  cin.tie(0);
+  cin >> n;
+  for(int i = 1; i <= n; i++) {
+    cin >> t[i] >> p[i];
+  }
+  for(int i = n; i > 0; i--) {
+    if(i + t[i] <= n+1) {
+      dp[i] = max(dp[i+t[i]]+p[i],dp[i+1]);
+    }
+    else {
+      dp[i] = dp[i+1];
+    }
+  }
+  cout << *max_element(dp+1,dp+n+1);
+}
