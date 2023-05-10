3ff910fa4db7f7e7e491d8257c61d08a5da365e9
hjm113 <63735064+hjm113@users.noreply.github.com>
2023-05-10 14:47:04 +0900

    [Bronze V] Title: Hook, Time: 0 ms, Memory: 17732 KB -BaekjoonHub

---
+++ 백준/Bronze/10189. Hook/Hook.cc
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <queue>
+#include <stack>
+#include <tuple>
+#include <deque>
+#include <string>
+#include <algorithm>
+#include <climits>
+#include <cmath>
+using namespace std;
+using ll = long long;
+#define X first
+#define Y second
+int n,m;
+int board[2005];
+int dp[2005][2005];
+int main(void){
+  ios::sync_with_stdio(0);
+  cin.tie(0);
+  cout << "#  # #### #### #  #\n#### #  # #  # # #\n#### #  # #  # # #\n#  # #### #### #  #";
+
+} 
