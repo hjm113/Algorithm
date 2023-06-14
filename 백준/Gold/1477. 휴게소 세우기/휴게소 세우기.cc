17b0b8506e319b2d74dad242765a9c13cb658d62
hjm113 <63735064+hjm113@users.noreply.github.com>
2023-06-14 10:36:20 +0900

    [Gold IV] Title: 휴게소 세우기, Time: 0 ms, Memory: 5928 KB -BaekjoonHub

---
+++ 백준/Gold/1477. 휴게소 세우기/휴게소 세우기.cc
@@ -0,0 +1,54 @@
+#include <iostream>
+#include <queue>
+#include <stack>
+#include <tuple>
+#include <deque>
+#include <string>
+#include <algorithm>
+#include <climits>
+#include <cmath>
+using namespace std;
+using ll = long long;
+#define X first
+#define Y second
+int n, m, l;
+int ary[55];
+int check[1000][1000];
+int solve(int x) {
+  int ans = 0;
+  for(int i = 1; i <= n+1; i++) {
+    int dist = ary[i]-ary[i-1];
+    int a = dist/x;
+    int b = dist%x;
+    if(b > 0) {
+      a++;
+    }
+    check[x][i] = a;
+    ans += (a-1);
+  }
+  return ans;
+}
+
+int main() {
+  ios::sync_with_stdio(0);
+  cin.tie(0);
+  cin >> n >> m >> l;
+  ary[0] = 0;
+  for(int i = 1; i <= n; i++) {
+    cin >> ary[i];
+  }
+  ary[n+1] = l;
+  int st = 1;
+  int en = l;
+  sort(ary,ary+n+2);
+  while(st < en) {
+    int mid = (st+en)/2;
+    if(solve(mid) > m) {
+      st = mid+1;
+    }
+    else if(solve(mid) <= m) {
+      en = mid;
+    }
+  }
+  cout << st;
+}
