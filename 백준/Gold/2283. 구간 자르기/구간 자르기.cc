dc6c966a65fc6b6cf14dfbab64e488f9b985b0ba
hjm113 <63735064+hjm113@users.noreply.github.com>
2023-06-16 10:40:46 +0900

    [Gold II] Title: 구간 자르기, Time: 500 ms, Memory: 5928 KB -BaekjoonHub

--- 백준/Gold/2283. 구간 자르기/구간 자르기.cc
+++ 백준/Gold/2283. 구간 자르기/구간 자르기.cc
@@ -9,7 +9,7 @@ using namespace std;
 using ll = long long;
 #define X first
 #define Y second
-pair<int,int> ary[1005];
+int num[1000005];
 int n, k;
 int main() {
   ios::sync_with_stdio(0);
@@ -18,42 +18,29 @@ int main() {
   for(int i = 0; i < n; i++) {
     int x, y;
     cin >> x >> y;
-    ary[i] = {x,y};
+    for(int i = x+1; i <= y; i++) {
+      num[i]++;
+    }
   }
-  
   int en = 0;
   int fst = 0;
-  int check = 0;
+  int sum = 0;
+  int mx = 0;
   for(int st = 0; st <= 1000000; st++) {
-    int sum = 0;
+    sum -= num[st];
     while(sum < k && en < 1000001) {
       en++;
-      sum = 0;
-      for(int i = 0; i < n; i++) {
-        int mn = max(ary[i].X,st);
-        int mx = min(ary[i].Y,en);
-        if(en >= ary[i].X) {
-          sum += mx-mn;
-        }
-        if(sum > k) {
-          break;
-        }
-      }
-      //cout << st << " " << en << " " << sum << "\n"; 
+      sum += num[en];
     }
-    if(en == 1000001) {
-      break;
-    }
+    //cout << st << " " << en << " " << sum << "\n";
     if(sum == k) {
-      check = 1;
       fst = st;
       break;
     }
-    en--;
-  }
-  if(check == 0) {
-    cout << 0 << " " << 0;
-    return 0;
+    if(en == 1000001) {
+      cout << 0 << " " << 0;
+      return 0;
+    }
   }
   cout << fst << " " << en;
 }
