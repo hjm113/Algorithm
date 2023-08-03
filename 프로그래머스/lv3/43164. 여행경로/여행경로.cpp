c9c63eb65f2d51eef4768ea7aa18fbfc71621142
hjm113 <63735064+hjm113@users.noreply.github.com>
2023-08-04 08:48:48 +0900

    [level 3] Title: 여행경로, Time: 0.10 ms, Memory: 10.5 MB -BaekjoonHub

--- 프로그래머스/lv3/43164. 여행경로/여행경로.cpp
+++ 프로그래머스/lv3/43164. 여행경로/여행경로.cpp
@@ -6,32 +6,10 @@
 #include <algorithm>
 using namespace std;
 map<string, int> mp;
-int used[10005][10005];
-string ary[10005];
 deque<string> des[10005]; 
-vector<string> answer;
-int cnt = 0;
-void dfs(int k, string ap, int n) {
-    if(k == n && cnt == 0) {
-        cnt++;
-        answer.push_back("ICN");
-        for(int i = 0; i < n; i++) {
-            answer.push_back(ary[i]);
-        }
-        return;
-    }
-    for(int i = 0; i < des[mp[ap]].size(); i++) {
-        if(used[mp[ap]][i] == 0) {
-            used[mp[ap]][i] = 1;
-            ary[k] = des[mp[ap]][i];
-            dfs(k+1,des[mp[ap]][i],n);
-            used[mp[ap]][i] = 0;
-        }
-    }
-}
-
 vector<string> solution(vector<vector<string>> tickets) {
-    int cnt = 1;
+    vector<string> answer;
+    int cnt = 0;
     for(int i = 0; i < tickets.size(); i++) {
         if(mp.find(tickets[i][0]) == mp.end()) {
             mp[tickets[i][0]] = cnt;
@@ -39,9 +17,22 @@ vector<string> solution(vector<vector<string>> tickets) {
         }
         des[mp[tickets[i][0]]].push_back(tickets[i][1]);
     }
+    for(int i = 0; i < 3; i++) {
+        for(int j = 0; j < des[i].size(); j++) {
+            cout << des[i][j] << " ";
+        }
+        cout << "\n";
+    }
     for(int i = 0; i < 10001; i++) {
         sort(des[i].begin(),des[i].end());
     }
-    dfs(0,"ICN",tickets.size());
+    string st = "ICN";
+    answer.push_back(st);
+    while(!des[mp[st]].empty()) {
+        answer.push_back(des[mp[st]].front());
+        string tmp = des[mp[st]].front();
+        des[mp[st]].pop_front();
+        st = tmp;
+    }
     return answer;
 }
