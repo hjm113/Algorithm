4b9fc5c837abc6ae80c9ac904b897910ad5f791e
hjm113 <63735064+hjm113@users.noreply.github.com>
2023-08-24 16:29:32 +0900

    [level 3] Title: 등굣길, Time: 0.08 ms, Memory: 3.82 MB -BaekjoonHub

---
+++ 프로그래머스/lv3/42898. 등굣길/등굣길.cpp
@@ -0,0 +1,32 @@
+#include <string>
+#include <vector>
+#include <iostream>;
+using namespace std;
+int dp[105][105];
+int check[105][105];
+int solution(int m, int n, vector<vector<int>> puddles) {
+    int answer = 0;
+    dp[1][1] = 1;
+    for(int i = 0 ; i < puddles.size(); i++) {
+        check[puddles[i][1]][puddles[i][0]] = 1;
+    }
+    for(int i = 1; i <= n; i++) {
+        for(int j = 1; j <= m; j++) {
+            if(check[i][j] == 1) {
+                continue;
+            }
+            dp[i][j] += (dp[i-1][j] + dp[i][j-1]);
+            dp[i][j] %= 1000000007;
+        }
+    }
+    /*
+    for(int i = 1; i <= n; i++) {
+        for(int j = 1; j <= m; j++) {
+            cout << dp[i][j] << " ";
+        }
+        cout << "\n";
+    }
+    */
+    answer = dp[n][m];
+    return answer;
+}
