9895bebb113d3be8f8801065bb60d48474e2b48b
hjm113 <63735064+hjm113@users.noreply.github.com>
2023-09-01 19:18:53 +0900

    [unrated] Title: 광물 캐기, Time: 0.47 ms, Memory: 4.15 MB -BaekjoonHub

--- 프로그래머스/unrated/172927. 광물 캐기/광물 캐기.cpp
+++ 프로그래머스/unrated/172927. 광물 캐기/광물 캐기.cpp
@@ -5,55 +5,54 @@
 using namespace std;
 int chk[5];
 int mx = 2000000000;
-int ans = 0;
 int ary[15];
 vector<string> mine;
-void dfs(int k, int u) {
-    if(k == u) {
-        int ans =0;
-        for(int i = 0; i < u; i++) {
-            for(int j = i*5; j < (i*5)+5; j++) {
-                if(j >= mine.size()) {
-                    break;
-                }
-                if(ary[i] == 0) {
-                    ans+= 1;
-                }
-                if(ary[i] == 1) {
-                    if(mine[j] == "diamond") {
-                        ans += 5;
-                    }
-                    else {
-                        ans += 1;
-                    }
-                }
-                if(ary[i] == 2) {
-                    if(mine[j] == "diamond") {
-                        ans += 25;
-                    }
-                    else if(mine[j] == "iron") {
-                        ans += 5;
-                    }
-                    else {
-                        ans += 1;
-                    }
-                }
-            }
-        }
+void dfs(int k, int used, int u, int ans) {
+    if(k == u+1) {
+        //cout << ans << "\n";
         if(ans < mx) {
             mx = ans;
         }
         return;
     }
-    /*
-    */
+    if(used != -1) {
+        for(int j = (k-1)*5; j < (k-1)*5+5; j++) {
+            if(j >= mine.size()) {
+                break;
+            }
+            if(used == 0) {
+                ans+= 1;
+            }
+            if(used == 1) {
+                if(mine[j] == "diamond") {
+                    ans += 5;
+                }
+                else {
+                    ans += 1;
+                }
+            }
+            if(used == 2) {
+                if(mine[j] == "diamond") {
+                    ans += 25;
+                }
+                else if(mine[j] == "iron") {
+                    ans += 5;
+                }
+                else {
+                    ans += 1;
+                }
+            }
+        }
+    }
     for(int i = 0; i < 3; i++) {
         if(chk[i] == 0) {
+            if(k == u) {
+                dfs(k+1,i,u,ans);
+            }
             continue;
         }
         chk[i]--;
-        ary[k] = i;
-        dfs(k+1,u);
+        dfs(k+1,i,u,ans);
         chk[i]++;
     }
 }
@@ -73,7 +72,7 @@ int solution(vector<int> picks, vector<string> minerals) {
         cnt += picks[i];
     }
     n = min(n,cnt);
-    dfs(0,n);
+    dfs(0,-1,n,0);
     answer = mx;
     return answer;
 }
