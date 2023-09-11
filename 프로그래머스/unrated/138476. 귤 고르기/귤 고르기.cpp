eb706dd4cc6b5083ac9a1f7477ee4b8a0b69aeff
hjm113 <63735064+hjm113@users.noreply.github.com>
2023-09-11 14:00:35 +0900

    [unrated] Title: 귤 고르기, Time: 8.37 ms, Memory: 7.14 MB -BaekjoonHub

--- 프로그래머스/unrated/138476. 귤 고르기/귤 고르기.cpp
+++ 프로그래머스/unrated/138476. 귤 고르기/귤 고르기.cpp
@@ -7,33 +7,25 @@ vector<int> ans;
 int solution(int k, vector<int> tangerine) {
     int answer = 0;
     sort(tangerine.begin(),tangerine.end());
-    int val = -1;
-    int cnt = 0;
-    int chk = 0;
-    for(int i = 0; i < tangerine.size(); i++) {
-        if(chk == 0) {
-            val = tangerine[i];
-            chk++;
-        }
+    int val = tangerine[0];
+    int cnt = 1;
+    for(int i = 1; i < tangerine.size(); i++) {
         if(val != tangerine[i]) {
             ans.push_back(cnt);
             cnt = 0;
             val = tangerine[i];
         }
-        cnt++;
         if(i == tangerine.size()-1) {
+            cnt++;
             ans.push_back(cnt);
         }
+        cnt++;
     }
     sort(ans.begin(),ans.end(),greater());
     int sum = 0;
-    /*
-    for(int i = 0; i < ans.size(); i++) {
-        cout << ans[i] << " ";
-    }
-    */
     for(int i = 0; i < ans.size(); i++) {
         sum += ans[i];
+        //cout << ans[i] << "\n";
         if(sum >= k) {
             answer = i+1;
             break;
