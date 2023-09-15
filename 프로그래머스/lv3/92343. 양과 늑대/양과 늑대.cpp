81ee3e91930f64a9b8f2e175de195fd8ec1cdfdf
hjm113 <63735064+hjm113@users.noreply.github.com>
2023-09-15 14:09:28 +0900

    [level 3] Title: 양과 늑대, Time: 0.03 ms, Memory: 3.72 MB -BaekjoonHub

---
+++ 프로그래머스/lv3/92343. 양과 늑대/양과 늑대.cpp
@@ -0,0 +1,63 @@
+#include <string>
+#include <vector>
+#include <iostream>
+using namespace std;
+vector<int> adj[20];
+vector<int> info;
+int mx = 0;
+int cnt = 0;
+void dfs(int cur, int sums, int sumw, vector<int> chk , vector<int> vis, int kind) {
+    vis[cur] = 1;
+    if(mx < sums) {
+        mx = sums;
+    }
+    for(auto nxt : adj[cur]) {
+        if(vis[nxt] == 1) {
+            continue;
+        }
+        vis[nxt] = 1;
+        if(chk[nxt] == 1) {
+            dfs(nxt, sums, sumw, chk, vis, 1);
+        }
+        if(chk[nxt] == 0 && info[nxt] == 0) {
+            sums++;
+            chk[nxt] = 1;
+            vector<int> tmp(17,0);
+            dfs(nxt, sums, sumw, chk, tmp, 0);
+        }
+        if(chk[nxt] == 0 && info[nxt] == 1) {
+            if(sums <= sumw+1) {
+                continue;
+            }
+            sumw++;
+            chk[nxt] = 1;
+            dfs(nxt, sums, sumw, chk, vis, 0);
+            chk[nxt] = 0;
+            sumw--;
+        }
+    }
+}
+
+int solution(vector<int> inf, vector<vector<int>> edges) {
+    int answer = 0;
+    for(int i = 0; i < edges.size(); i++) {
+        int a = edges[i][0];
+        int b = edges[i][1];
+        adj[a].push_back(b);
+        adj[b].push_back(a);
+    }
+    for(int i = 0; i < inf.size(); i++) {
+        info.push_back(inf[i]);
+    }
+    vector<int> chk;
+    vector<int> vis;
+    for(int i = 0; i < 17; i++) {
+        chk.push_back(0);
+        vis.push_back(0);
+    }
+    chk[0] = 1;
+    dfs(0, 1, 0, chk, vis, 0);
+    cout << mx;
+    answer = mx;
+    return answer;
+}
