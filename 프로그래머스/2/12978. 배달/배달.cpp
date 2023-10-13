467d940328bf1e9f6bd4fa26df1d157bdce60c0f
hjm113 <63735064+hjm113@users.noreply.github.com>
2023-10-13 14:18:10 +0900

    [level 2] Title: 배달, Time: 0.02 ms, Memory: 3.71 MB -BaekjoonHub

--- 프로그래머스/2/12978. 배달/배달.cpp
+++ 프로그래머스/2/12978. 배달/배달.cpp
@@ -1,44 +1,12 @@
 #include <iostream>
 #include <vector>
-#include <queue>
-#define X first
-#define Y second
 using namespace std;
-vector<pair<int,int>> adj[55];
-priority_queue<pair<int,int>, vector<pair<int,int>>, greater<pair<int,int>>> pq;
-int dist[55];
-int mx = 2000000000;
+
 int solution(int N, vector<vector<int> > road, int K) {
     int answer = 0;
-    for(int i = 0; i < road.size(); i++) {
-        int a = road[i][0];
-        int b = road[i][1];
-        int c = road[i][2];
-        adj[a].push_back({c,b});
-        adj[b].push_back({c,a});
-    }
-    for(int i = 0; i < 55; i++) {
-        dist[i] = mx;
-    }
-    dist[1] = 0;
-    pq.push({dist[1],1});
-    while(!pq.empty()) {
-        auto cur = pq.top();
-        pq.pop();
-        if(dist[cur.Y] != cur.X) {
-            continue;
-        }
-        for(auto nxt : adj[cur.Y]) {
-            if(dist[nxt.Y] > cur.X+nxt.X) {
-                dist[nxt.Y] = cur.X+nxt.X;
-                pq.push({cur.X+nxt.X,nxt.Y});
-            }
-        }
-    }
-    for(int i = 1; i <= N; i++) {
-        if(dist[i] <= K) {
-            answer++;
-        }
-    }
+
+    // [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
+    cout << "Hello Cpp" << endl;
+
     return answer;
 }
