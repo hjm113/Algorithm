1f62485f783f0b19f65f2f8f80d32e4457320bdc
hjm113 <63735064+hjm113@users.noreply.github.com>
2023-11-06 16:52:17 +0900

    [Gold II] Title: 골목 대장 호석 - 효율성 2, Time: 1960 ms, Memory: 131040 KB, Score: 43 point -BaekjoonHub

--- 백준/Gold/20183. 골목 대장 호석 － 효율성 2/골목 대장 호석 － 효율성 2.cc
+++ 백준/Gold/20183. 골목 대장 호석 － 효율성 2/골목 대장 호석 － 효율성 2.cc
@@ -17,12 +17,43 @@ priority_queue< pair<ll,ll>, vector<pair<ll, ll>>, greater<pair<ll,ll>>> pq;
 vector<pair<ll,ll>> adj[100005];
 ll dst[100005];
 ll den[100005];
-ll mst[100005];
-ll men[100005];
+int pst[100005];
+int pen[100005];
 int n, m, a, b;
 ll c;
-ll mx = 9000000000000000;
-ll ans = 9000000000000000;
+ll mx = (1LL<<63)-1;
+ll ans = (1LL<<63)-1;
+vector<int> pos;
+
+int solve(int st, int en, ll limit) {
+  vector<ll> d(100005, mx);
+  d[st] = 0;
+  pq.push({0,st});
+  while(!pq.empty()) {
+    auto cur = pq.top();
+    pq.pop();
+    if(d[cur.Y] != cur.X) {
+      continue;
+    }
+    if(cur.Y == en) {
+      return 0;
+    }
+    for(auto nxt : adj[cur.Y]) {
+      if(nxt.X > limit) {
+        continue;
+      }
+      if(nxt.X+cur.X > c) {
+        continue;
+      }
+      if(d[nxt.Y] > nxt.X+cur.X) {
+        d[nxt.Y] = nxt.X+cur.X;
+        pq.push({nxt.X+cur.X,nxt.Y});
+      }
+    }
+  }
+  return 1;
+}
+
 int main(int argc, char** argv) {
   ios::sync_with_stdio(0);
   cin.tie(0);
@@ -34,55 +65,20 @@ int main(int argc, char** argv) {
     adj[st].push_back({mon,en});
     adj[en].push_back({mon,st});
   }
-  for(int i = 1; i <= n; i++) {
-    den[i] = mx;
-    dst[i] = mx;
-  }
-  dst[a] = 0;
-  mst[a] = 0;
-  pq.push({0,a});
-  while(!pq.empty()) {
-    auto cur = pq.top();
-    pq.pop();
-    if(dst[cur.Y] != cur.X) {
-      continue;
+  ll st = 0;
+  ll en = 1000000001;
+  while(st < en) {
+    ll mid = (st+en)/2;
+    if(solve(a,b,mid) == 0) {
+      en = mid;
     }
-    for(auto nxt : adj[cur.Y]) {
-      if(dst[nxt.Y] > nxt.X+cur.X) {
-        dst[nxt.Y] = nxt.X+cur.X;
-        mst[nxt.Y] = max(nxt.X,mst[cur.Y]);
-        pq.push({nxt.X+cur.X,nxt.Y});
-      }
+    else {
+      st = mid+1;
     }
   }
-  den[b] = 0;
-  men[b] = 0;
-  pq.push({0,b});
-  while(!pq.empty()) {
-    auto cur = pq.top();
-    pq.pop();
-    if(den[cur.Y] != cur.X) {
-      continue;
-    }
-    for(auto nxt : adj[cur.Y]) {
-      if(den[nxt.Y] > nxt.X+cur.X) {
-        den[nxt.Y] = nxt.X+cur.X;
-        men[nxt.Y] = max(nxt.X,men[cur.Y]);
-        pq.push({nxt.X+cur.X,nxt.Y});
-      }
-    }
-  }
-  for(int i = 1; i <= n; i++) {
-    if(dst[i]+den[i] <= c) {
-      ll over = max(men[i],mst[i]);
-      if(ans > over) {
-        ans = over;
-      }
-    }
-  }
-  if(ans == mx) {
+  if(st == 1000000001) {
     cout << -1;
     return 0;
   }
-  cout << ans;
+  cout << st;
 }
